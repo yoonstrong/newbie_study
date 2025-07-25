cc3e77e2daee5cecb232f72aa1b5953d637a56ea
yoonstrong <khyoon0507@gmail.com>
2025-07-25 10:25:00 +0900

    algo

---
+++ algorithm/윤강한/swea_1545_reverse_D1.py
@@ -0,0 +1,8 @@
+주어진 숫자부터 0까지 순서대로 찍어보세요.
+
+```python
+n = int(input())
+
+for i in range(n, -1, -1) :
+    print(i, end = ' ')
+```
---
+++ algorithm/윤강한/swea_2027_crossline_D1.py
@@ -0,0 +1,8 @@
+주어진 텍스트를 그대로 출력하세요
+
+```py
+
+for i in range(6) :
+    plus = list("+++++")
+    plus[i] = '#'
+    print(plus)
