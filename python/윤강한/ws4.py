f8737afd754a0e52993cbbd73141f8f40fb3a047
윤강한 <154643346+yoonstrong@users.noreply.github.com>
2025-07-27 01:36:13 +0900

    Update ws4.py
    5번 도저히 모르겠어요

--- python/윤강한/ws4.py
+++ python/윤강한/ws4.py
@@ -6,7 +6,6 @@ from utils import create_url
 NAME = 'develper'
 MAIN_URL = 'http://127.0.0.1:8000'
 
-
 def create_url(name, main_url, page_num=1):
     new_url = f'{main_url}/{name}?page={page_num}'
     return new_url
